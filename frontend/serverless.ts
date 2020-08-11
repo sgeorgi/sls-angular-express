e9de600d980265c0e3430c01cf6e07a4dcdb5bc8
Sebastian  Georgi <sebastian@georgi.io>
2020-08-11 15:26:25 +0200

    Frontend: Implemented creation of Certs and Domains for 3 stages

--- frontend/serverless.ts
+++ frontend/serverless.ts
@@ -7,22 +7,29 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
 export const app = express();
+
 const distFolder = join(process.cwd(), 'dist/frontend/browser');
 const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
   bootstrap: AppServerModule,
 }));
+
 app.set('view engine', 'html');
 app.set('views', distFolder);
+
 // Example Express Rest API endpoints
-app.get('/api/**', (req, res) => { });
+// app.get('/api/**', (req, res) => { });
 // Serve static files from /browser
+
 app.get('*.*', express.static(distFolder, {
   maxAge: '1y'
 }));
+
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
   res.render(indexHtml, {req, providers: [{provide: APP_BASE_HREF, useValue: req.baseUrl}]});
 });
+
 export * from './src/main.server';
