495c34a0841b8899a7ad3a7c6b5471d335436fb4
Sebastian Georgi <sgeorgi@sgeorgi.de>
2020-08-11 23:16:32 +0200

    Fucked up double BasePath configuration and probably have to go for sls and sls-api

--- backend/src/main/scala/io/georgi/sls/ApiGatewayResponse.scala
+++ backend/src/main/scala/io/georgi/sls/ApiGatewayResponse.scala
@@ -1,4 +1,4 @@
-package io.sgeorgi.sls
+package io.georgi.sls
 
 import scala.beans.BeanProperty
 
---
+++ backend/src/main/scala/io/georgi/sls/Handler.scala
@@ -0,0 +1,17 @@
+package io.georgi.sls
+
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import org.apache.logging.log4j.{LogManager, Logger}
+
+import scala.jdk.CollectionConverters._
+
+class Handler extends RequestHandler[Request, ApiGatewayResponse] {
+
+  val logger: Logger = LogManager.getLogger(getClass)
+
+  def handleRequest(input: Request, context: Context): ApiGatewayResponse = {
+    logger.info(s"Received a request: $input")
+    val headers = Map("x-custom-response-header" -> "my custom response header value")
+    ApiGatewayResponse(200, "Go Serverless v2.0! Your function executed successfully!", headers.asJava, true)
+  }
+}
